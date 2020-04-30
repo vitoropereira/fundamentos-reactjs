0b386459104069b2622ccbd94016ea897111348c
VITOR PEREIRA <vop1234@hotmail.com>
2020-04-30 08:13:36 -0300

    First Day

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,16 +11,21 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
+
   <Container size={size}>
     <header>
       <img src={Logo} alt="GoFinances" />
       <nav>
         {
-          // Todo
+          <>
+            <Link to="/" >Listagem</Link>
+            <Link to="/Import" >Importar</Link>
+          </>
         }
       </nav>
     </header>
   </Container>
-);
+
+)
 
 export default Header;
